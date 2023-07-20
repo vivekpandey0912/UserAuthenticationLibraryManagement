90e389562e6b5f5313630e71bdc5e4b1c848b414
Vivek Pandey <vp1446392@gmail.com>
2023-07-21 02:39:14 +0530

    lombok annotations added for required fields

--- src/main/java/com/niit/bej/userauthenticationservice/domain/User.java
+++ src/main/java/com/niit/bej/userauthenticationservice/domain/User.java
@@ -1,12 +1,22 @@
 package com.niit.bej.userauthenticationservice.domain;
 
 
+import lombok.*;
+
 import javax.persistence.Entity;
 
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 @Entity
 public class User {
 
-
-
+    private int emailId;
+    private String password;
+    private String userType;
 
 }

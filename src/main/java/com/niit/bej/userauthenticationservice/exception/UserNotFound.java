b3b03bf64e93ffc9c58d273f67f36de4514a5f0a
Vivek Pandey <vp1446392@gmail.com>
2023-07-21 02:43:52 +0530

    User defined exception added UserNotFound.java

--- src/main/java/com/niit/bej/userauthenticationservice/exception/UserNotFound.java
+++ src/main/java/com/niit/bej/userauthenticationservice/exception/UserNotFound.java
@@ -1,4 +1,12 @@
 package com.niit.bej.userauthenticationservice.exception;
 
-public class UserNotFound {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User Not Found Exception")
+public class UserNotFound extends Exception {
+    public UserNotFound(String message) {
+        super(message);
+    }
 }

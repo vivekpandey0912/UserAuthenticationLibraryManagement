337a522ddeb81ecf5cab4c6aa2f696c1f7b7d0e4
Vivek Pandey <vp1446392@gmail.com>
2023-07-21 02:56:08 +0530

    Annotated with @RestController to created restful APIt

--- src/main/java/com/niit/bej/userauthenticationservice/controller/UserController.java
+++ src/main/java/com/niit/bej/userauthenticationservice/controller/UserController.java
@@ -1,6 +1,13 @@
 package com.niit.bej.userauthenticationservice.controller;
 
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+
+
 
 
 }

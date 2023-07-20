75cdf828337e273c58e3d094d4ba6c094b801d42
Vivek Pandey <vp1446392@gmail.com>
2023-07-21 02:51:53 +0530

    two method added in UserService.java

--- src/main/java/com/niit/bej/userauthenticationservice/service/UserService.java
+++ src/main/java/com/niit/bej/userauthenticationservice/service/UserService.java
@@ -1,6 +1,12 @@
 package com.niit.bej.userauthenticationservice.service;
 
+import com.niit.bej.userauthenticationservice.domain.User;
+
 public interface UserService {
+
+
+    public User RegisterUser(User user);
+    public User loginUser(User user);
 
 
 

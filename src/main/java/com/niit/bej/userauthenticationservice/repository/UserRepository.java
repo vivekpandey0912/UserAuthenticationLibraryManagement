961a1b90f048e196ea5ef3832187cfd5c0392595
Vivek Pandey <vp1446392@gmail.com>
2023-07-21 02:49:14 +0530

    custom method added in repository interface and extending JPA Repository

--- src/main/java/com/niit/bej/userauthenticationservice/repository/UserRepository.java
+++ src/main/java/com/niit/bej/userauthenticationservice/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.niit.bej.userauthenticationservice.repository;
 
-public interface UserRepository {
+import com.niit.bej.userauthenticationservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User,String> {
+
+    User findUserByEmailIdAndPasswordAndUserType(int emailId, String password, String userType);
+
+
 }

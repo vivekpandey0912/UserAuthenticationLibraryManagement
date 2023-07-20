be89a2536e08b2ad5d5f14d7763fb4d4014382d0
Vivek Pandey <vp1446392@gmail.com>
2023-07-21 02:52:46 +0530

    created UserServiceImpl to implement interfaces method

---
+++ src/main/java/com/niit/bej/userauthenticationservice/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.niit.bej.userauthenticationservice.service;
+
+import com.niit.bej.userauthenticationservice.domain.User;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User RegisterUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User loginUser(User user) {
+        return null;
+    }
+}
